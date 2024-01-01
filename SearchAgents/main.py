dc4b1456aba4c1b2eaf3afda13a6e35a68b6b0a8
a-b-h-a-y-s-h-i-n-d-e <theabhay.shinde@gmail.com>
2024-01-01 14:41:40 +0530

    Fix some issues!

--- SearchAgents/main.py
+++ SearchAgents/main.py
@@ -6,33 +6,58 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import re
 import time
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException, NoSuchWindowException
+
+
 
 def askForValue():
     # value = input("Enter the value ! -> ")
     value = "ca/san%20jose"
     return value
 
+def fetchNumberOfResults(driver, agentsPage):
+    string = driver.find_element(By.XPATH, "//div[@class='col-6 col-md-4 col-l-4 col-xl-4']")
+    print(string.text)
+    number_match = re.search(r'\b\d+\b', string.text)
 
-def loading_full_page(driver, agentsPage, wait):
-    try:
-        agentsPage.find_elements(By.XPATH, "//div[@class='FindAgentRoute__row']")
-        wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='FindAgentRoute__row']")))
+    # Check if a match is found
+    if number_match:
+        number_of_agents = int(number_match.group())
+        print(f"Number of agents: {number_of_agents}")
+        return number_of_agents
+    else:
+        print("No match found.")
 
-    except Exception as e:
-        print(e)
 
+
+
+def loading_full_page(driver, agentsPage, wait, searchResults):
+    while True:
+        try:
+            agents = agentsPage.find_elements(By.XPATH, "//div[@class='FindAgentRoute__row']")
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            if len(agents) == searchResults:
+                break
+
+            wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='FindAgentRoute__row']")))
+
+
+        except Exception as e:
+            print(e)
+    print(len(agents))
+    iterateEachCard(driver, agentsPage, wait, agents)
+
+
+
+def iterateEachCard(driver, agentsPage, wait, agents):
+    # now iterating through each card
     
+    for agent in agents:
+        pass
 
-
-
-# def iterateEachCard(driver, agentsPage, wait):
-#     # now iterating through each card
-    
-#     pass
-    
     
     
 value = askForValue()
@@ -48,6 +73,8 @@ actions = ActionChains(driver)
 wait = WebDriverWait(driver, 10)
 
 
+
+    
 try:
     agentsPage = wait.until(
         EC.presence_of_element_located((By.XPATH, "//div[@class='infinite-scroll-component KWInfiniteScroll row']"))
@@ -58,8 +85,10 @@ try:
 except Exception as e:
     print(e)
 
+searchResults= fetchNumberOfResults(driver, agentsPage)
+print(searchResults)
 
-loading_full_page(driver, agentsPage, wait)
+# loading_full_page(driver, agentsPage, wait, searchResults)
 
 
 
